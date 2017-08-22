dfbf1a3c6b4e89a99192dd95c628b301f8af3277
Lucifer <lucifer.hsu@macrowell.com.tw>
2017-08-22 15:04:56 +0800

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 
 var bot = linebot({
   channelId:1530656843,
-  channelSecret:2e821d557b98b077497ae5b5ad1d9b95,
+  channelSecret:"2e821d557b98b077497ae5b5ad1d9b95",
   channelAccessToken:oTEtHgzuiDtqd61VAfQebC2W6neeYUVicjRGo1nB+tgwUt7ySj9MYzHkVhF+Po3DYQWyWrPnromZWRID37Cwl3cq47TOn680VteO43JMHnsEjPMBh6HyPK/xmA0J/sdfqcB3FqZrIhLyJUnlOFKILwdB04t89/1O/w1cDnyilFU=
 });
 
@@ -20,3 +20,5 @@ var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
+
+	
